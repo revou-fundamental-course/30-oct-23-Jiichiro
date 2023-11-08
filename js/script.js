a0eae0066e73e3e891436da768e60e2c44b9e755
Jiichiro <cokmesoh@gmail.com>
2023-11-08 21:13:10 +0700

    reapir code

--- js/script.js
+++ js/script.js
@@ -34,19 +34,19 @@ function hitungBMI() {
     //memilih pesan sesuai hasil dari perhitungan BMI
     if (BMI < 18.5) {
         clearInterval(typing);
-        pesan.value = "kekurangan Berat Badan"
+        pesan.textContent = "kekurangan Berat Badan"
         typeWriter("Salam sehat! Saya berharap pesan ini menemui Anda dalam keadaan baik. Saya ingin mengingatkan Anda pentingnya memperhatikan kesehatan tubuh, termasuk asupan nutrisi yang cukup. Segera dapatkan saran dari ahli gizi untuk membantu Anda mencapai berat badan yang sehat dan membangun kekuatan tubuh dengan cara yang benar.")
     } else if (BMI >= 18.5 && BMI <= 24.9) {
         clearInterval(typing);
-        pesan.value = "Nomal (ideal)"
+        pesan.textContent = "Nomal (ideal)"
         typeWriter("Salam sehat! Saya senang melihat Anda menjaga keseimbangan berat badan Anda dengan baik. Teruskan pola makan dan gaya hidup sehat Anda, dan pastikan untuk tetap aktif secara fisik. Ingatlah bahwa kesehatan adalah kekayaan terbesar kita.")
     } else if (BMI >= 25 && BMI <= 29.9) {
         clearInterval(typing);
-        pesan.value = "Kelebihan Berat Badan"
+        pesan.textContent = "Kelebihan Berat Badan"
         typeWriter("Salam sehat! Saya ingin memberikan dukungan kepada Anda dalam perjalanan menuju kehidupan sehat. Pertahankan semangat dan lakukan langkah kecil menuju tujuan kesehatan Anda. Konsultasikan dengan ahli gizi atau profesional kesehatan untuk mendapatkan saran yang sesuai untuk mengelola berat badan dengan bijak.")
     } else if (BMI >= 30) {
         clearInterval(typing);
-        pesan.value = 'Kegemukan (Obesitas)';
+        pesan.textContent = 'Kegemukan (Obesitas)';
         typeWriter("Salam sehat! Saya ingin mengatakan bahwa Anda tidak sendirian dalam perjuangan untuk mencapai keadaan tubuh yang lebih sehat. Pertama-tama, saya ingin mengatakan bahwa Anda berharga dan berarti, tidak peduli ukuran atau bentuk tubuh Anda. Namun, penting bagi kita semua untuk memprioritaskan kesehatan. Saya mendorong Anda untuk mencari bantuan dari profesional kesehatan atau ahli gizi yang dapat membimbing Anda menuju kehidupan yang lebih sehat. Ingat, setiap langkah kecil menuju kesehatan adalah pencapaian besar.")
     }
 }
